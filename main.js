18479843c18636d7648f1bd748b405e44e27a6c1
Arghadwip Sarkar <68644790+arghadwip23@users.noreply.github.com>
2020-09-26 13:12:45 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ $('form').submit(function(event) {
   event.preventDefault();
   $('#photo').empty();
   var search = $('#search').val();
-  var url = 'http://api.unsplash.com/search/photos?query='+search+'&client_id=bHBu7kGXLQQl9taGTM7Dm0jndWbqohyAUPoT1vr0DEM&per_page=35';
+  var url = 'http://api.unsplash.com/search/photos?query='+search+'&client_id=4CsRSQrzR-BWipkoi--Hf1cVskuRxToT-dhzsoWKm6s&per_page=35';
   
   $.ajax({
     method: 'GET',
